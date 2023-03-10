0e659e843ac9bfc48a8a0562df6c9e4bb8d395f3
TheShadowFR <117437326+TheShadowFR@users.noreply.github.com>
2023-03-10 10:47:31 +0100

    v1

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <time.h>
 
+// takuzu
 
 // 1 - creer une fonction qui prend la variable solution, et la variable tableauJoueur (par exemple) : un tableau rempli de 0, cette fonction doit copier certaines valeurs (grace au rand() % N) 
 // de solution dans tableauJoueur afin d'initialiser une grille jouable ( car solution est bel est bien la solution, donc pas jouable par définition. ).
@@ -16,6 +17,72 @@
     // - la fonction verifierGrille (qui fait ceci pour toute la grille et permet de definir la condition de victoire)
 
 // 4 - Ecrire le code principal qui permet de jouer au joueur :  le programme prend fin quand on gagne 
+int generer(int solution[8][8], int tableauJoueur[8][8]){
+
+        for(int i=0;i<8; ++i){
+            for(int j=0; j<8; ++j){  
+                int hazard = rand()%2;
+                if (hazard){
+                    tableauJoueur[i][j] = solution[i][j];  
+                }
+            }
+        }
+        return 0;
+    }
+
+int afficher(int tableau[8][8]){
+    printf("---------------------------------");  
+    printf("\n");
+    for(int i=0;i<8; ++i){
+        for(int j=0; j<8; ++j){  
+            printf("%d ", tableau[i][j]);
+        }
+        printf("\n");
+    }
+    printf("---------------------------------");  
+    printf("\n");
+}
+
+int verifierLigneColone(int tab[8][8], int saisieX, int saisieY, int reponse){
+    printf("je verifie ligne/colone %d %d %d %d", tab[8][8], saisieX, saisieY, reponse);
+    return 0;
+}
+
+int verifierGrille(int tab[8][8]){
+    printf("je verifie grille %d", tab[8][8]);
+    return 0;
+}
+int saisir(int tableau[8][8]){
+    int saisieX = 0;
+    int saisieY = 0;
+    int reponse = 0;
+    int tabVerif[8][8];
+
+    printf("Choisissez les coordonnees x/y de la case a modifier: ");
+    scanf("%d %d", &saisieX, &saisieY);
+    printf("%d %d\n", saisieX, saisieY);
+    printf("Choisisser entre 1 et 0: ");
+    scanf("%d", &reponse);
+    printf("%d\n", reponse);
+
+    for(int i=0;i<8; ++i){
+        for(int j=0; j<8; ++j){  
+            tabVerif[i][j] = tableau[i][j];
+        }
+    }
+    tabVerif[saisieX][saisieY] = reponse;
+    for(int i=0;i<8; ++i){
+        for(int j=0; j<8; ++j){  
+            printf("%d ", tabVerif[i][j]);
+        }
+        printf("\n");
+    }
+    verifierLigneColonne(tabVerif, saisieX, saisieY, reponse);
+    verifierGrille(tabVerif);
+    return 0;
+}
+
+
 
 int main(){
     // Ne pas toucher le code entre les commentaires
@@ -50,7 +117,28 @@ int main(){
     printf("---------------------------------");  
     printf("\n");  
     //toucher le code entre les commentaires 
+    int tableauJoueur[8][8] = {
+            {3,3,3,3, 3,3,3,3},
+            {3,3,3,3, 3,3,3,3},
+            {3,3,3,3, 3,3,3,3},
+            {3,3,3,3, 3,3,3,3},
+        
+            {3,3,3,3, 3,3,3,3},
+            {3,3,3,3, 3,3,3,3},
+            {3,3,3,3, 3,3,3,3},
+            {3,3,3,3, 3,3,3,3}
+        };
 
+    generer(solution, tableauJoueur);
+    while (true)
+    {
+        afficher(tableauJoueur);
+        saisir(tableauJoueur);
+    }
+    
+    
+    
+    
 
 
     //Ne pas toucher au code ci dessous
