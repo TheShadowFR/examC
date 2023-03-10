6de37fdcbad0e8c56e95dfc30047aa2e696ecc13
TheShadowFR <117437326+TheShadowFR@users.noreply.github.com>
2023-03-10 13:53:24 +0100

    vf

--- main.c
+++ main.c
@@ -17,23 +17,206 @@
     // - la fonction verifierGrille (qui fait ceci pour toute la grille et permet de definir la condition de victoire)
 
 // 4 - Ecrire le code principal qui permet de jouer au joueur :  le programme prend fin quand on gagne 
-int generer(int solution[8][8], int tableauJoueur[8][8]){
-
-        for(int i=0;i<8; ++i){
-            for(int j=0; j<8; ++j){  
-                int hazard = rand()%2;
-                if (hazard){
+int generer(int solution[8][8], int tableauJoueur[8][8], char difficulte){
+        if (difficulte == 'Y'){
+            for(int i=0;i<8; ++i){
+                for(int j=0; j<8; ++j){  
                     tableauJoueur[i][j] = solution[i][j];  
                 }
             }
+            tableauJoueur[rand()%8][rand()%8] = 3;
+        }else{
+            for(int i=0;i<8; ++i){
+                for(int j=0; j<8; ++j){  
+                    int hazard = rand()%2;
+                    if (hazard){
+                        tableauJoueur[i][j] = solution[i][j];  
+                    }
+                }
+            }
+            return 0;
         }
+    }
+int verifierTroisMemeChiffreLigne(int tab[8][8]){
+    for (int i=0; i<8; i++){
+        for (int j=0; j <8; j++){
+            int memeCaractere = 0;
+            for (int k = 0; k < 3; k++)
+            {
+                if (tab[i][j+k] == tab[i][j]){
+                    memeCaractere++;
+                }
+                if (memeCaractere == 3){
+                    return 0;
+                }
+            }
+        }
+    }
+    return 1;
+}
+int verifierTroisMemeChiffreColonne(int tab[8][8]){                                     //ok
+    for (int i=0; i<8; i++){
+        for (int j=0; j <8; j++){
+            int memeCaractere = 0;
+            for (int k = 0; k < 3; k++)
+            {
+                if (tab[i+k][j] == tab[i][j]){
+                    memeCaractere++;
+                }
+                if (memeCaractere == 3){
+                    return 0;
+                }
+            }
+        }
+    }
+    return 1;
+}
+int verifLigne(int tab[8][8]){
+    int memeLigne = 0;
+    for(int i = 0; i < 8 ; i++){
+        for(int j = i+1 ; j < 8 ; j++)
+        {
+            for(int k = 0; k < 8 ; k++){
+                if (tab[i][k] == tab[j][k]){
+                        memeLigne++;
+                }
+            }
+        }
+    }
+    if (memeLigne == 8){
+        return 1;
+    }else{
         return 0;
     }
+    
+}
+int verifColonne(int tab[8][8]){
+    int memeColonne = 0;
+    for(int i = 0; i < 8 ; i++){
+        for(int j = i+1 ; j < 8 ; j++)
+        {
+            for(int k = 0; k < 8 ; k++){
+                if (tab[i][k] == tab[j][k]){
+                        memeColonne++;
+                }
+            }
+        }
+    }
+    if (memeColonne == 8){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+int verifierNbChiffreLigne(int tab[8][8]){                                         //ok
+    int nbligneValide = 0;
+    for (int i=0; i<8; i++){
+        int nb1 = 0;
+        int nb0 = 0;
+        for (int j=0; j <8; j++){
+            if (tab[i][j] == 0){
+                nb0++;
+            }
+            if (tab[i][j] == 1){
+                nb1++;
+            }
+        }
+        if (nb1 == nb0){
+            nbligneValide++;
+        }
+    }
+    if (nbligneValide == 8){
+        return 1;
+    }
+    return 0;
+}
+int verifierNbChiffreColone(int tab[8][8]){                                                      // ok
+    int nbColoneValide = 0;
+    for (int i=0; i<8; i++){
+        int nb1 = 0;
+        int nb0 = 0;
+        for (int j=0; j <8; j++){
+            if (tab[j][i] == 0){
+                nb0++;
+            }
+            if (tab[j][i] == 1){
+                nb1++;
+            }
+        }
+        if (nb1 == nb0){
+            nbColoneValide++;
+        }
+    }
+    if (nbColoneValide == 8){
+        return 1;
+    }
+    return 0;
+}
+
+
+
+int verifierLigneColonne(int tab[8][8]){
+    int verifValide = 0;
+    if (verifierTroisMemeChiffreLigne(tab)){
+        verifValide++;
+        printf("verifierTroisMemeChiffreLigne\n");
+    }    
+    if (verifierTroisMemeChiffreColonne(tab)){
+        verifValide++;
+        printf("verifierTroisMemeChiffreColonne\n");
+    }
+    if (verifLigne(tab)){
+        verifValide++;
+        printf("verifLigne\n");
+    }
+    if (verifColonne(tab)){
+        verifValide++;
+        printf("verifColonne\n");
+    }
+    if (verifierNbChiffreLigne(tab)){
+        verifValide++;
+        printf("verifierNbChiffreLigne\n");
+    }
+    if (verifierNbChiffreColone(tab)){
+        verifValide++;
+        printf("verifierNbChiffreColone\n");
+    }
+
+        
+    // verifValide = 6;
+    printf("%d verif(s) ont etait valide(s)\n", verifValide);
+        //verifierColone
+    if (verifValide == 6){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+int verifierGrille(int tab[8][8]){
+    int grilleNonComplete = 0;
+    for (int i = 0; i <8; i++){
+        for (int j = 0; j<8; j++){
+            if (tab[i][j] == 3){
+                grilleNonComplete++;
+            }
+        }
+
+    }
+    if (grilleNonComplete == 0){
+        return 1;
+    }else{
+        return 0;
+    }
+}
 
 int afficher(int tableau[8][8]){
     printf("---------------------------------");  
     printf("\n");
-    for(int i=0;i<8; ++i){
+    printf("   0 1 2 3 4 5 6 7\n");
+    printf("   _______________\n");
+    for(int i= 0;i<8; ++i){
+        printf("%d |", i);
         for(int j=0; j<8; ++j){  
             printf("%d ", tableau[i][j]);
         }
@@ -43,43 +226,31 @@ int afficher(int tableau[8][8]){
     printf("\n");
 }
 
-int verifierLigneColone(int tab[8][8], int saisieX, int saisieY, int reponse){
-    printf("je verifie ligne/colone %d %d %d %d", tab[8][8], saisieX, saisieY, reponse);
-    return 0;
-}
 
-int verifierGrille(int tab[8][8]){
-    printf("je verifie grille %d", tab[8][8]);
-    return 0;
-}
-int saisir(int tableau[8][8]){
+void saisir(int tableau[8][8]){
     int saisieX = 0;
     int saisieY = 0;
     int reponse = 0;
-    int tabVerif[8][8];
+    int gagnant = 0;
 
     printf("Choisissez les coordonnees x/y de la case a modifier: ");
     scanf("%d %d", &saisieX, &saisieY);
-    printf("%d %d\n", saisieX, saisieY);
     printf("Choisisser entre 1 et 0: ");
     scanf("%d", &reponse);
-    printf("%d\n", reponse);
 
-    for(int i=0;i<8; ++i){
-        for(int j=0; j<8; ++j){  
-            tabVerif[i][j] = tableau[i][j];
-        }
+    tableau[saisieX][saisieY] = reponse;
+
+    if (verifierLigneColonne(tableau)){
+        gagnant++;
     }
-    tabVerif[saisieX][saisieY] = reponse;
-    for(int i=0;i<8; ++i){
-        for(int j=0; j<8; ++j){  
-            printf("%d ", tabVerif[i][j]);
-        }
-        printf("\n");
+    if (verifierGrille(tableau)){
+        gagnant++;
     }
-    verifierLigneColonne(tabVerif, saisieX, saisieY, reponse);
-    verifierGrille(tabVerif);
-    return 0;
+    if (gagnant == 2){
+        printf("Bienjoue vous avez gagne !!!!!!");
+        exit(0);
+    }
+
 }
 
 
@@ -117,29 +288,27 @@ int main(){
     printf("---------------------------------");  
     printf("\n");  
     //toucher le code entre les commentaires 
-    int tableauJoueur[8][8] = {
-            {3,3,3,3, 3,3,3,3},
-            {3,3,3,3, 3,3,3,3},
-            {3,3,3,3, 3,3,3,3},
-            {3,3,3,3, 3,3,3,3},
-        
-            {3,3,3,3, 3,3,3,3},
-            {3,3,3,3, 3,3,3,3},
-            {3,3,3,3, 3,3,3,3},
-            {3,3,3,3, 3,3,3,3}
-        };
 
-    generer(solution, tableauJoueur);
+    int tableauJoueur[8][8] = {
+        {3,3,3,3, 3,3,3,3},
+        {3,3,3,3, 3,3,3,3},
+        {3,3,3,3, 3,3,3,3},
+        {3,3,3,3, 3,3,3,3},
+    
+        {3,3,3,3, 3,3,3,3},
+        {3,3,3,3, 3,3,3,3},
+        {3,3,3,3, 3,3,3,3},
+        {3,3,3,3, 3,3,3,3}
+    };
+    char Rep;
+    printf("Voulez vous un tableau facile ? (Y/N): ");
+    scanf("%c", &Rep);
+    generer(solution, tableauJoueur, Rep);
     while (true)
     {
         afficher(tableauJoueur);
         saisir(tableauJoueur);
     }
-    
-    
-    
-    
-
 
     //Ne pas toucher au code ci dessous
     system("pause");
